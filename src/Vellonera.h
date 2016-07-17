7f828d73ac1b78d5692f092e59003998776f874a
angel sancio <angel sancio>
2016-07-16 21:47:36 -0400

    Implement methods

---
+++ src/Vellonera.h
@@ -0,0 +1,12 @@
+#include <cstddef>
+#include <algorithm>
+#include <random>
+#include <memory>
+#include <string>
+#include <deque>
+#include <map>
+#include <SFML/Audio.hpp>
+
+class Vellonera{
+//TODO: Implements the methods
+}
